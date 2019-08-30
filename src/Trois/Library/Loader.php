a067ce50aa00da74e1bd1b6c7d59c6ef0c7087ec
fubin.feng <feng-fubin@shanghaihomeexpo.com>
2019-08-30 14:56:07 +0800

    my first composer of project

---
+++ src/Trois/Library/Loader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FDG367
+ * Date: 2019/8/30
+ * Time: 14:51
+ */
+namespace Trois\Library;
+
+class Loader
+{
+    public function index()
+    {
+        echo 'this is my first composer of project';
+    }
+}
